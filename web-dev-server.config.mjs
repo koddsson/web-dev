b551058404835091dd5be90a7152e021a19ba104
Kristján Oddsson <koddsson@gmail.com>
2022-10-27 17:13:58 +0200

    Setup PostCSS and `open-props`

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,9 +1,7 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
+import {esbuildPlugin} from '@web/dev-server-esbuild'
 
 export default {
   watch: true,
   nodeResolve: true,
-  rootDir: "./src/",
-  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
-};
-
+  plugins: [esbuildPlugin({ts: true, target: 'auto'})]
+}
