7a6fc5da0b227618e120517aade5eb7e3079d384
Kristján Oddsson <koddsson@gmail.com>
2023-07-04 22:27:08 +0200

    Use my own eslint config and CSS plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-import postcssJitProps from 'postcss-jit-props'
-import OpenProps from 'open-props'
-import atImport from 'postcss-import'
+import postcssJitProps from 'postcss-jit-props';
+import OpenProps from 'open-props';
+import atImport from 'postcss-import';
+import nano from 'cssnano';
 
 export default {
-  plugins: [atImport(), postcssJitProps(OpenProps)]
+  plugins: [atImport(), postcssJitProps(OpenProps), nano({preset: 'default'})]
 }
